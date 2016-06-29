268550f4f3f6e23f66a81082cf5bc28d3e0c1edb
Trey50Daniel <tagg8945@gmail.com>
2016-06-28 21:11:18 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/example/tagg8/threescreensapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.tagg8.threescreensapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
