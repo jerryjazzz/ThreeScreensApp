de159a407f5b630774317279e613aa63c3427c5f
Trey50Daniel <tagg8945@gmail.com>
2016-07-06 09:56:07 -0500

    Only allowed confirmation number mod 0 to be a valid entry.

--- app/src/main/java/com/example/tagg8/threescreensapp/ConfirmationActivity.java
+++ app/src/main/java/com/example/tagg8/threescreensapp/ConfirmationActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.*;
@@ -12,6 +13,10 @@ import java.util.*;
 
 public class ConfirmationActivity extends AppCompatActivity {
     private int phoneNum;
+    private EditText editConfirm;
+    private EditText editName;
+    private int confirmNum;
+    private String confirmName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +24,20 @@ public class ConfirmationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_confirmation);
         Intent intent = getIntent();
         phoneNum = (int)intent.getSerializableExtra("phoneNum");
+        editConfirm = (EditText) findViewById(R.id.enter_code);
+        editName = (EditText) findViewById(R.id.enter_name);
+        confirmName = editName.getText().toString();
         String phoneText = String.valueOf(phoneNum);
         Log.v("Phone Text:", phoneText);
 
     }
 
     public void confirmClick(View view) {
-        int remainder = phoneNum % 17;
+        String yourUrl = "";
+        confirmNum = Integer.valueOf(editConfirm.getText().toString());
+        int remainder = confirmNum % 17;
         if(remainder == 0) {
-
+            yourUrl += confirmName + phoneNum;
         }
         else {
             Toast.makeText(ConfirmationActivity.this, "Not a valid confirmation number!",
